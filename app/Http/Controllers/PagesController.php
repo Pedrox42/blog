815d3917bbb0c7c91cefc6dcb57fafe803fdd123
Pedrox42 <pedroclufjf@gmail.com>
2021-01-10 06:00:59 -0300

    Limpando codigo

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -21,9 +21,4 @@ class PagesController extends Controller
         return view('dashboard', compact('posts', 'lastPost', 'i'));
     }
 
-    public function material($id)
-    {
-        
-    }
-
 }
